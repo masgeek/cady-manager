a403f4e8384c3c5f8ce2072f9cef268642445a84
masgeek <barsamms@gmail.com>
2026-08-07 15:00:02 +0300

    chore(eslint): add TypeScript parser for `.ts` and `.tsx` file support

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,14 @@
+import tsParser from '@typescript-eslint/parser';
+
 export default [
   {
     ignores: ['**/dist/', '**/node_modules/', '**/.turbo/'],
   },
   {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+    },
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'off',
